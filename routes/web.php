5a92a15e13ba056ec88b26b6cca3c7216bb0bfbf
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-06-29 16:29:21 +0300

    test Restrictions on multiple route params

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,19 @@ Route::get('/', function () {
     return 'main web-site page';
 });
 
-Route::get('/post/{id}', function ($id) {
-    return 'Post id is ' . $id;
-})->where('id', '[0-9]+');
+Route::get('/post/{slug}/{id}', function ($slug, $id) {
+    return 'Post id is ' . $id . ', and Post slug is ' . $slug;
+})->where('slug', '[a-z0-9_-]+')->where('id', '[0-9]+');
 
-Route::get('/user/{id}', function ($id) {
-    return 'User id is ' . $id;
-})->where('id', '[0-9]+');
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return 'User id is ' . $id . ', and User name is ' . $name;
+})->where('id', '[0-9]+')->where('name', '[a-z_-]{3,}');
+
+
+Route::get('/posts/{date}', function ($date) {
+    return 'Post date is ' . $date;
+})->where('date', '[0-9]{4,4}-[0-9]{2,2}-[0-9]{2,2}');
+
+Route::get('/user/{order}', function ($order) {
+    return 'User order is ' . $order;
+})->whereIn('order',['name', 'surname', 'age']);
