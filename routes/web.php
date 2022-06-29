306d7a56349f4e3d0bc78229164a93c9461e807f
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-06-29 17:56:19 +0300

    test Resolving Route Conflicts

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,14 @@ Route::get('/city/{name}', function ($name) {
     return 'City name is ' . $name;
 })->whereAlpha('name');
 
+Route::get('/user/all', function () {
+		return 'all';
+	});
+
+Route::get('/user/all/desc', function () {
+		return 'all desc';
+	});
+
+Route::get('/user/{name}/{id?}', function ($name, $id) {
+		return 'name id';
+	});
