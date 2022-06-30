d36bc1a5adf3eb52e20e91449c8ef4e3d301a430
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-06-30 08:28:13 +0300

    test Passing data to a View

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,16 +3,8 @@
 
 	class PostController extends Controller
 	{
-         public function show($id)
-		{
-			$posts = [
-				1 => 'text 1',
-				2 => 'text 2',
-				3 => 'text 3',
-				4 => 'text 4',
-				5 => 'text 5',
-			];
-			
-			return $posts[$id];
+         public function show()
+	        {
+			return view('test', ['name' => 'Yanush', 'surname' => 'Polishchuk']);
 		}
 	}
---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>my view</title>
+	</head>
+	<body>
+		Author`s name: {{ $name }}
+		Author`s surname: {{ $surname }}
+	</body>
+</html>
