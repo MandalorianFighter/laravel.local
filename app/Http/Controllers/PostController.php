74450948432274c78e3e6f975b14a448f6e185d5
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-07-11 11:57:05 +0300

    test database builder data inserting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,17 @@ class PostController extends Controller
         public function show()
         {
 		//$posts = DB::table('posts')->get();
+                DB::table('posts')->insert([
+		[
+			'title' => 'page1',
+			'slug'  => 'slug1',
+		],
+		[
+			'title' => 'page2',
+			'slug'  => 'slug2',
+		]
+	        ]);
                 $posts = DB::table('posts')
-		->whereIdOrSlug(1,'post-3')
 		->get();
                 return view('post.show', ['posts' => $posts]);
         }
