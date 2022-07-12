d6e29decc009c5738c9f2b0a09a2dffb5cd5d8d7
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-07-12 11:16:58 +0300

    test database eloquent records creating

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,12 @@ class PostController extends Controller
         public function show()
         {
 		//$posts = DB::table('posts')->get();
+                $post = new Post;
+	
+	        $post->title = 'new_post_title';
+	        $post->slug  = 'new_post_text';
+	
+	        $post->save();
                 $posts = Post::all();
                 return view('post.show', ['posts' => $posts]);
         }
