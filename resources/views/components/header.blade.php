4ce9ba34e3077701ea7531ff8c5c32a3a9a483fc
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-07-18 18:23:08 +0300

    test components passing view data

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,10 @@
 <header>
-header
 <x-logo />
 <x-contact />
+
+    <a href="{{ route('main') }}">Home</a>
+    <a href="{{ route('posts.index') }}">Posts</a>
+    <a href="{{ route('users.index') }}">Users</a>
+<p>{{ $var1 }}</p>
+<p>{{ $var2 }}</p>
 </header>
