f25c4489fdc64a3c4d6583801dd84039e4c4bbd4
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-07-19 17:17:41 +0300

    test redirect named routes with parameters

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 
         public function get1()
     {
-      return redirect()->route('categories');
+      return redirect()->route('categories.show', ['id' => 1]);
     }
 
         public function index(Request $request)
